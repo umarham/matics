69a9baae7dff6daa212f22e6b8f3c2d57c7f686f
Umar <45851120+umarham@users.noreply.github.com>
2022-02-24 19:06:41 +0100

    Update hardhat.config.js

--- cool-domains/hardhat.config.js
+++ cool-domains/hardhat.config.js
@@ -5,8 +5,8 @@ require('dotenv').config();
   solidity: "0.8.10",
   networks: {
 		mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/RxhvX8Q8uXePh2LjFYRRQT-vR2zXW7j5",
-      accounts: ["caaade16055208e251882eebac45a2bea5642c6dba4b6ca39bf910a8fadfbd3b"],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
 		}
   }
 };
